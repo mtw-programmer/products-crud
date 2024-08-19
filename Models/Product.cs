8237a350426ebf2f678e48e1ac4b70604fd49c7d
mtw-programmer <visnia58@gmail.com>
2024-08-19 19:10:36 +0200

    feat: create Product model

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+namespace ProductsCrudApi.Models
+{
+  public class Product
+  {
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required decimal Price { get; set; }
+  }
+}
